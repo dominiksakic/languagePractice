a4d31d310b592f115144c4330f7cb014cc5c4410
dominiksakic <dominik.sakic@yahoo.com>
2025-07-20 08:01:27 +0900

    chore: add error handling. - try reading file, except FNF. - clean up code and read files from the stdin.

--- main.py
+++ main.py
@@ -2,19 +2,21 @@ import os
 import sys
 
 def Python(args=[]): 
-    if(len(args) > 1):
+    if len(args) > 1:
         print("Usage: plox [script]")
         os._exit(64)
-    elif (len(args) == 1):
-            with open("file", "r") as f:
-                print(f"file: {f}")
-                print("run function")
+    elif len(args) == 1:
+        try:
+            with open(args[0], "r") as f:
+                code = f.read()
+                print(code)
+        except FileNotFoundError:
+            print(f"File not found: {args[0]}")
+            os._exit(66)
     else:
         for line in sys.stdin:
-            if line == None:
-                break
-            print(line)
+            print(line, end="")
 
 
 if __name__ == "__main__":
-    Python()
+    Python(sys.argv[1:])
