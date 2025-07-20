aca088512819b171cdbb36f1f8fd28dbe9628779
dominiksakic <dominik.sakic@yahoo.com>
2025-07-20 10:39:05 +0900

    feat: add type hints.

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import os 
 import sys
 
-def Python(args=[]): 
+def Python(args: list[str]) -> None: 
     if len(args) > 1:
         print("Usage: plox [script]")
         os._exit(64)
     elif len(args) == 1:
-        run_file(argv[0])
+        run_file(args[0])
     else:
         run_repl()
 
-def run_file(path):
+def run_file(path: str) -> None:
     try:
         with open(path, "r") as f:
             code = f.read()
@@ -19,11 +19,11 @@ def run_file(path):
         print(f"File not found: {args[0]}")
         os._exit(66)
 
-def run_repl():
+def run_repl() -> None:
     while True:
         try:
             print("> ", end="", flush=True)
-            line = sys.stdin.readline().strip()
+            line: str = sys.stdin.readline().strip()
             if not line:
                 break # EOF (Ctrl + D)
             if line == "exit":
@@ -35,14 +35,14 @@ def run_repl():
             print("\nExiting on Keyboard interuppt")
             sys.exit(0)
 
-def run(source):
+def run(source: str) -> None:
     for token in source:
         print(token)
 
-def error(line, message):
-    report(line, "", mesesge)
+def error(line: int, message: str) -> None:
+    report(line, "", messasge)
 
-def report(line, where, message):
+def report(line: int, whern: str, message: str) -> None:
     print(f"[line {line}] Error {where} : {message}")
 
 if __name__ == "__main__":
