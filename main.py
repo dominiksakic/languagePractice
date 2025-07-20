f8029e79207ad7d52ce97889ceee19792aaa8d5d
dominiksakic <dominik.sakic@yahoo.com>
2025-07-20 10:31:53 +0900

    feat: add REPL and Running files. - add error handling helper functions.

--- main.py
+++ main.py
@@ -6,17 +6,44 @@ def Python(args=[]):
         print("Usage: plox [script]")
         os._exit(64)
     elif len(args) == 1:
-        try:
-            with open(args[0], "r") as f:
-                code = f.read()
-                print(code)
-        except FileNotFoundError:
-            print(f"File not found: {args[0]}")
-            os._exit(66)
+        run_file(argv[0])
     else:
-        for line in sys.stdin:
-            print(line, end="")
+        run_repl()
 
+def run_file(path):
+    try:
+        with open(path, "r") as f:
+            code = f.read()
+            run(code) 
+    except FileNotFoundError:
+        print(f"File not found: {args[0]}")
+        os._exit(66)
+
+def run_repl():
+    while True:
+        try:
+            print("> ", end="", flush=True)
+            line = sys.stdin.readline().strip()
+            if not line:
+                break # EOF (Ctrl + D)
+            if line == "exit":
+                print("Exiting REPL")
+                sys.exit(0)
+            else:
+                run(line)
+        except KeyboardInterrupt:
+            print("\nExiting on Keyboard interuppt")
+            sys.exit(0)
+
+def run(source):
+    for token in source:
+        print(token)
+
+def error(line, message):
+    report(line, "", mesesge)
+
+def report(line, where, message):
+    print(f"[line {line}] Error {where} : {message}")
 
 if __name__ == "__main__":
     Python(sys.argv[1:])
